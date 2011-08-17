6e4599607d46e07ff2685382e1d9954a28841a17
Dave Harrington <harrington.dave@gmail.com>
2011-08-17 01:28:15 +0100

    Moved to janus install

--- install.py
+++ install.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python
 import os
 import os.path
-ignores = ['.git', 'README', 'install.py']
-for file in os.listdir("."):
-	if not file in ignores:
-		original = os.path.abspath(file)
-		new = os.path.join(os.environ['HOME'], file)
-		os.symlink(original, new)
-		print "Symlinked %s -> %s" % (original, new)
-#test
+import sys
+import subprocess
+
+force = True if '-f' in sys.argv else False
+
+ignores = ['.git', 'README', 'install.py', 'vim_shortcuts.txt']
+dir_list = os.listdir(".")
+print dir_list
+for file in dir_list:
+
+    if not file in ignores:
+        original = os.path.abspath(file)
+        new = os.path.join(os.environ['HOME'], file)
+
+        if force or os.path.islink(new):
+            print 'Removing old: ', new
+            os.remove(new)
+
+        print "Symlinking %s -> %s" % (original, new),
+        try:
+            os.symlink(original, new)
+        except:
+            print original, "FAIL"
+        else:
+            print "SUCCESS"
+
+print "Installing Janus"
+subprocess.call(["git", "submodule", "init"])
+subprocess.call(["git", "submodule", "update"])
+subprocess.call(["rake"], cwd="./.vim")
