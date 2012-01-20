23ef13ef2dc558ac07cd02b2b83eef09ebc84da6
Dave Harrington <dave@lightbox.com>
2012-01-20 15:26:13 +0000

    more improvements

--- install.py
+++ install.py
@@ -2,19 +2,17 @@
 import os
 import os.path
 import sys
-import subprocess
 
 force = True if '-f' in sys.argv else False
 
 ignores = ['.git', 'README', 'install.py', 'vim_shortcuts.txt']
 dir_list = os.listdir(".")
-print dir_list
 for file in dir_list:
 
     if not file in ignores:
         original = os.path.abspath(file)
-				if not file[0] == '.':
-					file = '.' + file
+        if not file[0] == '.':
+            file = '.' + file
         new = os.path.join(os.environ['HOME'], file)
 
         if force or os.path.islink(new):
@@ -28,8 +26,3 @@ for file in dir_list:
             print original, "FAIL"
         else:
             print "SUCCESS"
-
-print "Installing Janus"
-subprocess.call(["git", "submodule", "init"])
-subprocess.call(["git", "submodule", "update"])
-subprocess.call(["rake"], cwd=".vim")
